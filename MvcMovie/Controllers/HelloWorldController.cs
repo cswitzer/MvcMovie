fbe821833971d026fe742fff68ef24838d2371f9
Chase <chasehillig@yahoo.com>
2022-06-15 13:41:28 -0600

    init commit

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Welcome(string name, int ID = 1)
+        {
+            // populate view data with params from url before rendering view
+            ViewData["Message"] = "Hello " + name;
+            ViewData["ID"] = ID;
+
+            return View();
+        }
+    }
+}
